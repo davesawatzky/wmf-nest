705f1180dedd72e245b834e40fd177c96d2404f6
David Sawatzky <david@diatonic.io>
2023-10-26 16:53:31 -0500

    set cors to true

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
     rawBody: true,
   })
 
@@ -32,14 +33,14 @@ async function bootstrap() {
     })
   )
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    optionsSuccessStatus: 200,
-    credentials: true,
-    preflightContinue: false,
-    maxAge: 1000 * 60 * 60 * 24, // 1 day
-  })
+  // app.enableCors({
+  //   origin: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   optionsSuccessStatus: 200,
+  //   credentials: true,
+  //   preflightContinue: false,
+  //   maxAge: 1000 * 60 * 60 * 24, // 1 day
+  // })
 
   app.use(cookieParser())
   app.useGlobalPipes(
